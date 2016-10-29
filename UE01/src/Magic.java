1f67a7882140fb83f599b3e50acae1637d235da4
Tobias Hahn <morowinger@gmail.com>
2016-10-29 19:23:27 +0200

    final solution

--- UE01/src/Magic.java
+++ UE01/src/Magic.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.io.*;
 import java.util.*;
 import java.util.List;
@@ -8,7 +9,8 @@ import java.util.List;
  */
 public class Magic {
     private static myClustering means = new myClustering();
-    private static String[] colors = {"blue", "green", "red", "yellow", "black"};
+    private static myVarianceClustering meansBike = new myVarianceClustering();
+    private static String[] colors = {"blue", "green", "red", "yellow", "black", "purple"};
     private static String[] label = {"a", "b", "c", "d", "e", "f", "g"};
 
     public static void main(String[] args) throws IOException {
@@ -32,6 +34,72 @@ public class Magic {
         centroids.add(new Point(40,50));
         centroids.add(new Point(100,50));
         printResult("mars/result4", means.clusterOne(points, centroids));
+
+        List<Point2D> points_bike = readDataBike("/home/tobias/repos/tohahn/UE_ML/UE01/data/fahrrad.csv");
+        printResultBike("bike/results1", meansBike.cluster(points_bike, 6));
+
+        ArrayList<Map<Point2D, List<Point2D>>> results = new ArrayList<>();
+        Map<Integer, Double> cost = new HashMap<>();
+        Map<Integer, Double> sil = new HashMap<>();
+        double min = Double.MAX_VALUE;
+        int index = -1;
+
+        Map<Point2D, List<Point2D>> sils = new HashMap<>();
+        Point2D silsC = new Point2D.Float(0,0);
+        sils.put(silsC, new ArrayList<>());
+        for (int i = 0; i < 19; i++) {
+            results.add(meansBike.cluster(points_bike, i+2));
+            cost.put(i, calculateCost(i, results.get(i)));
+            sil.put(i, calcSilhoutte(results.get(i)));
+
+            if (cost.get(i) < min) {
+                min = cost.get(i);
+                index = i;
+            }
+
+            sils.get(silsC).add(new Point2D.Float(sil.get(i).floatValue(), cost.get(i).floatValue()));
+        }
+
+        printResultBike("bike/result2", results.get(index));
+        printResultBike("bike/result4", sils);
+    }
+
+    private static double calculateCost(int num, Map<Point2D, List<Point2D>> clustering) {
+        float error = 0;
+        for (Point2D c : clustering.keySet()) {
+            for (Point2D p : clustering.get(c)) {
+                error += c.distance(p);
+            }
+        }
+        return 38.63767077587896/12 * num + error/2;
+    }
+
+    private static double calcSilhoutte(Map<Point2D, List<Point2D>> clustering) {
+        List<Point2D> centroids = new ArrayList<>();
+        List<Point2D> instances = new ArrayList<>();
+
+        centroids.addAll(clustering.keySet());
+        clustering.values().forEach(l -> instances.addAll(l));
+
+        double sil = 0, dA, dB;
+        for (Point2D p : instances) {
+            Point2D tempC = assign(p, centroids);
+            centroids.remove(tempC);
+            Point2D tempC2 = assign(p, centroids);
+            centroids.add(tempC);
+
+            dA = tempC.distance(p);
+            dB = tempC2.distance(p);
+
+            sil += (dB - dA) / Math.max(dA, dB);
+        }
+        sil /= instances.size();
+
+        return sil;
+    }
+
+    private static Point2D assign(Point2D instance, List<Point2D> centroids) {
+        return centroids.stream().min((p1, p2) -> Double.compare(p1.distance(instance), p2.distance(instance))).get();
     }
 
     private static void printResult(String name, Map<Point, List<Point>> result) throws IOException {
@@ -41,15 +109,41 @@ public class Magic {
         int i = 0;
         for (Point p : result.keySet()) {
             if (i > 0) tex.write(",");
-            tex.write(p.x+"_"+p.y+"c={mark=x,draw=" + colors[i%colors.length] + "},");
-            tex.write(p.x+"_"+p.y+"p={mark=o,draw=" + colors[i++%colors.length] + "}");
+            tex.write(p.getX()+"_"+p.getY()+"c={mark=x,draw=" + colors[i%colors.length] + "},");
+            tex.write(p.getX()+"_"+p.getY()+"p={mark=o,draw=" + colors[i++%colors.length] + "}");
         }
         tex.write("}]\n\\addplot[scatter,only marks,scatter src=explicit symbolic]table[meta=label] {\nx y label\n");
         for (Map.Entry<Point, List<Point>> e : result.entrySet()) {
             tex.write(e.getKey().x + " " + e.getKey().y + " " + e.getKey().x+"_"+e.getKey().y + "c\n");
             e.getValue().forEach(cP -> {
                 try {
-                    tex.write(cP.x + " " + cP.y + " " + e.getKey().x+"_"+e.getKey().y + "p\n");
+                    tex.write(cP.getX() + " " + cP.getY() + " " + e.getKey().x+"_"+e.getKey().y + "p\n");
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            });
+        }
+        tex.write("};\n\\end{axis}\n\\end{tikzpicture}\n\\end{document}");
+        tex.flush();
+        tex.close();
+    }
+
+    private static void printResultBike(String name, Map<Point2D, List<Point2D>> result) throws IOException {
+        BufferedWriter tex = new BufferedWriter(new FileWriter("/home/tobias/repos/tohahn/UE_ML/UE01/results/" + name + ".tex"));
+
+        tex.write("\\documentclass{article}\n\\usepackage{tikz,pgfplots}\n\\begin{document}\n\\begin{tikzpicture}\n\\begin{axis}[scatter/classes={");
+        int i = 0;
+        for (Point2D p : result.keySet()) {
+            if (i > 0) tex.write(",");
+            tex.write(p.getX()+"_"+p.getY()+"c={mark=x,draw=" + colors[i%colors.length] + "},");
+            tex.write(p.getX()+"_"+p.getY()+"p={mark=o,draw=" + colors[i++%colors.length] + "}");
+        }
+        tex.write("}]\n\\addplot[scatter,only marks,scatter src=explicit symbolic]table[meta=label] {\nx y label\n");
+        for (Map.Entry<Point2D, List<Point2D>> e : result.entrySet()) {
+            tex.write(e.getKey().getX() + " " + e.getKey().getY() + " " + e.getKey().getX()+"_"+e.getKey().getY() + "c\n");
+            e.getValue().forEach(cP -> {
+                try {
+                    tex.write(cP.getX() + " " + cP.getY() + " " + e.getKey().getX()+"_"+e.getKey().getY() + "p\n");
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
@@ -69,6 +163,19 @@ public class Magic {
         while ((line = csv.readLine()) != null) {
             points.add(new Point(Integer.parseInt(line.split(",")[0]), Integer.parseInt(line.split(",")[1])));
         }
+        return points;
+    }
+
+    private static List<Point2D> readDataBike(String name) throws IOException {
+        BufferedReader csv = new BufferedReader(new FileReader(name));
+        csv.readLine();
+
+        String line;
+        ArrayList<Point2D> points = new ArrayList<>();
+        while ((line = csv.readLine()) != null) {
+            points.add(new Point2D.Float(Float.parseFloat(line.split(",")[1]), Float.parseFloat(line.split(",")[2])));
+        }
+
         return points;
     }
 }
--- UE01/src/myVarianceClustering.java
+++ UE01/src/myVarianceClustering.java
@@ -1,13 +1,13 @@
-import java.awt.*;
 import java.awt.geom.Point2D;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by tobias on 29.10.16.
  */
 public class myVarianceClustering {
-    public double distance(Point2D.Float instance, Point2D.Float centroid) {
+    public double distance(Point2D instance, Point2D centroid) {
         return instance.distance(centroid);
     }
 
@@ -29,7 +29,54 @@ public class myVarianceClustering {
         return centroids.stream().min((p1, p2) -> Double.compare(p1.distance(instance), p2.distance(instance))).get();
     }
 
-    public Map<Point2D, List<Point2D>> cluster(List<Point2D> instances, List<Point2D> centroids) {
-        return null;
+    public Map<Point2D, List<Point2D>> cluster(List<Point2D> instances, int number) {
+        List<Point2D> centroids = getInitialCentroids(instances, number);
+        final Map<Point2D, List<Point2D>> clusters = new HashMap<>();
+        centroids.forEach(c -> {
+           clusters.put(c, new ArrayList<>());
+        });
+        double oldError = Double.MAX_VALUE, newError = 0;
+
+        while (oldError != newError) {
+            oldError = newError;
+            centroids.forEach(c -> {
+                clusters.remove(c);
+                clusters.put(c, new ArrayList<>());
+            });
+            instances.forEach(p -> clusters.get(assign(p, centroids)).add(p));
+
+            Map<Point2D, List<Point2D>> temp = new HashMap<>();
+            temp.putAll(clusters);
+            for (Map.Entry<Point2D, List<Point2D>> e : temp.entrySet()) {
+                Point2D newCentroid = centroid(e.getKey(), e.getValue());
+                if (!newCentroid.equals(e.getKey())) {
+                    List<Point2D> tempList = clusters.get(e.getKey());
+                    clusters.remove(e.getKey());
+                    centroids.remove(e.getKey());
+                    clusters.put(newCentroid, tempList);
+                    centroids.add(newCentroid);
+                }
+            }
+            newError = 0;
+            for (Point2D c : centroids) {
+               for (Point2D p : clusters.get(c)) {
+                   newError += distance(c,p);
+               }
+            }
+        }
+        return clusters;
+    }
+
+    private List<Point2D> getInitialCentroids(List<Point2D> instances, int number) {
+        Map<Integer, List<Point2D>> assign = new HashMap<>();
+
+        for (int i = 0; i < number; i++) {
+            assign.put(i, new ArrayList<>());
+        }
+        for (int i = 0; i < instances.size(); i++) {
+            assign.get(i%number).add(instances.get(i));
+        }
+
+        return assign.values().stream().map(l -> centroid(null, l)).collect(Collectors.toList());
     }
 }
