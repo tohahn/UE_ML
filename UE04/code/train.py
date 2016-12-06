f1c7fa4913a097389ef0a4d7325fbcb180ba72e9
Tobias Hahn <morowinger@gmail.com>
2016-12-06 01:12:48 +0100

    almost finished last exercise from exercise 4

---
+++ UE04/code/train.py
@@ -0,0 +1,12 @@
+import football as f
+
+if __name__ == "__main__":
+	ole = f.Expression("Ole")
+	tor = f.Expression("Toor!")
+	oh = f.Expression("Ohhh")
+	
+	f.recognize(ole)
+	f.recognize(ole)
+	f.recognize(oh)
+	f.recognize(ole)
+	f.recognize(tor)
