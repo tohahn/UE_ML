9d50eee88168730e7f5fb84d97646d1083e901ee
ramonleiser <ramonleiser@ramonleisersmbp.speedportentry2090126200030>
2017-01-25 16:45:14 +0100

    got expected apriori results from weka

--- UE06/code/src/MagicMain.java
+++ UE06/code/src/MagicMain.java
@@ -37,6 +37,18 @@ public class MagicMain {
 		 return returner;
 		 
 	}
+	/**
+	 * WEKA gets the following results:
+	 * 1. Wein=True 2 ==> Cola=True 2    <conf:(1)> lift:(1.8) lev:(0.1) [0] conv:(0.89)
+ 	 * 2. Wein=True 2 ==> Saft=True 2    <conf:(1)> lift:(2.25) lev:(0.12) [1] conv:(1.11)
+ 	 * 3. Bier=True Saft=True 2 ==> Cola=True 2    <conf:(1)> lift:(1.8) lev:(0.1) [0] conv:(0.89)
+ 	 * 4. Saft=True Wein=True 2 ==> Cola=True 2    <conf:(1)> lift:(1.8) lev:(0.1) [0] conv:(0.89)
+ 	 * 5. Cola=True Wein=True 2 ==> Saft=True 2    <conf:(1)> lift:(2.25) lev:(0.12) [1] conv:(1.11)
+ 	 * 6. Wein=True 2 ==> Cola=True Saft=True 2    <conf:(1)> lift:(3) lev:(0.15) [1] conv:(1.33)
+ 	 * 7. Bier=True 4 ==> Cola=True 3    <conf:(0.75)> lift:(1.35) lev:(0.09) [0] conv:(0.89)
+ 	 * 8. Saft=True 4 ==> Cola=True 3    <conf:(0.75)> lift:(1.35) lev:(0.09) [0] conv:(0.89)
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ArrayList<HashSet<Item>> transactions = new ArrayList<>();
 		for(Item[] transaction : transactionsArr){
